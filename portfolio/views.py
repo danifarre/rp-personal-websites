39f71b6eaf812e789af3f1128a34ed036f3e36a0
Daniel Farre <danifarre97@gmail.com>
2020-03-22 14:17:07 +0100

    Portfolio App completed

---
+++ portfolio/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Project
+
+def project_list(request):
+	projects = Project.objects.all()
+
+	context = {
+		'projects' : projects
+	}
+
+	return render(request, 'project_list.html', context)
+
+def project_detail(request, pk):
+	project = Project.objects.get(pk = pk)
+
+	context = { 'project': project }
+
+	return render(request, 'project_detail.html', context)
